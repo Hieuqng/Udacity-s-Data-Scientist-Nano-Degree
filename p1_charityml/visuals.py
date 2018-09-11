4a7e00b405b74491bbec8ae30f7d2750ec88cf8c
Hieu Nguyen <hieuqng26.email@gmail.com>
2018-09-10 19:46:17 -0500

    Modify some answers and visualization

--- p1_charityml/visuals.py
+++ p1_charityml/visuals.py
@@ -127,7 +127,7 @@ def feature_plot(importances, X_train, y_train):
     values = importances[indices][:5]
 
     # Creat the plot
-    fig = plt.figure(figsize = (9,5))
+    fig = plt.figure(figsize = (12,10))
     plt.title("Normalized Weights for First Five Most Predictive Features", fontsize = 16)
     plt.bar(np.arange(5), values, width = 0.6, align="center", color = '#00A000', \
           label = "Feature Weight")
